f5541d9660c9e0452e8f64ddf2656fd6922bbf99
t_jimbo <tjimbo152@gmail.com>
2025-12-16 21:09:54 +0900

    2-3 経過報告

---
+++ mynews/src/main/java/com/jutjoy/domain/form/news/NewsCreateForm.java
@@ -0,0 +1,20 @@
+package com.jutjoy.domain.form.news;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class NewsCreateForm {
+
+    @NotEmpty(message = "＊タイトルは必ず入力してください。")
+    @Size(max = 20, message = "＊タイトルは20文字以内で設定してください。")
+    private String title;
+
+    @NotEmpty(message = "＊本文は必ず入力してください。")
+    private String content;
+
+    private MultipartFile image;
+}
